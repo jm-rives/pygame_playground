1404db0b7f6c6a18ac9dbfd43e309597325e0d97
Joanna M. Rives <joanna.rives@gmail.com>
2018-07-31 14:55:34 -0700

    ADD system font.

--- simple_game1.py
+++ simple_game1.py
@@ -1,5 +1,7 @@
 #! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
 # -*- coding: utf-8 -*-
+# from the book "Instant Pygame for Python Game Development How-to" by Ivan Idris
+# accessed at https://www.packtpub.com/mapt/book/game_development/9781782162865 July 31, 2018.
 import pygame, sys
 from pygame.locals import *
 
@@ -10,4 +12,5 @@ screen = pygame.display.set_mode((400, 300))
 pygame.display.set_caption("Hello World!")
 
 while True:
-    pass
+    sys_font = pygame.font.Sysfont("None", 19)
+    
