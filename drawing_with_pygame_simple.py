721163029046ea78ed1531c1c203182ca457aa67
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-01 12:00:28 -0700

    INITIALIZE colors, add notes on numpy installation.

--- drawing_with_pygame_simple.py
+++ drawing_with_pygame_simple.py
@@ -1,2 +1,7 @@
 #! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
 # -*- coding: utf-8 -*-
+# was returning error but I had not downloaded numpy
+# still returning error, had to install numpy via https://stackoverflow.com/questions/17443354/install-numpy-on-python3-3-install-pip-for-python3
+import numpy
+
+colors = numpy.random.randint(0, 255, size=(4, 3))
