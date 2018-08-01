2795f36c7c770fe02911b47232750bf60bdec6a5
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-01 12:07:30 -0700

    ADD additional modules.

--- drawing_with_pygame_simple.py
+++ drawing_with_pygame_simple.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 # was returning error but I had not downloaded numpy
 # still returning error, had to install numpy via https://stackoverflow.com/questions/17443354/install-numpy-on-python3-3-install-pip-for-python3
+import pygame, sys
+from pygame.locals import *
 import numpy
 
 colors = numpy.random.randint(0, 255, size=(4, 3))
 
 WHITE = (255, 255, 255) # QUESTION? Would hex codes werk?
+
+screen.fill(WHITE)
