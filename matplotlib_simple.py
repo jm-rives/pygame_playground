5f50426865f05a5dbe20104a77be56aa10140e7b
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-02 15:12:29 -0700

    SET window caption, load image, add coordinates.

--- matplotlib_simple.py
+++ matplotlib_simple.py
@@ -29,3 +29,28 @@ def plot(data):
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((400, 400))
+
+pygame.display.set_caption('Animating Objects')
+img = pygame.image.load('head.jpg')
+
+steps = numpy.linspace(20, 360, 40).astype(int)
+right = numpy.zeros((2, len(steps)))
+down = numpy.zeros((2, len(steps)))
+left = numpy.zeros((2, len(steps)))
+
+right[0] = steps
+right[1] = 20
+
+down[0] = 360
+down[1] = steps
+
+left[0] = steps[::-1]
+left[1] = 360
+
+up[0] = 20
+up[1] = steps[::-1]
+
+pos = numpy.concatenate((right.T, down.T, left.T, up.T))
+i = 0
+history = numpy.array([])
+surf = plot(history)
