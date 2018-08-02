a0ab254d2aa6e4b458bc33135130fefc3542d331
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-02 10:06:20 -0700

    ADD reference.

--- animation_objects_simple.py
+++ animation_objects_simple.py
@@ -1,2 +1,4 @@
 #! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
 # -*- coding: utf-8 -*-
+# from the book "Instant Pygame for Python Game Development How-to" by Ivan Idris
+# accessed at https://www.packtpub.com/mapt/book/game_development/9781782162865 July 31, 2018.
