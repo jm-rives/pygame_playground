327d7024167d06aee29c553194e5e689f0a23af7
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-02 10:09:32 -0700

    IMPORT modules.

--- animation_objects_simple.py
+++ animation_objects_simple.py
@@ -2,3 +2,6 @@
 # -*- coding: utf-8 -*-
 # from the book "Instant Pygame for Python Game Development How-to" by Ivan Idris
 # accessed at https://www.packtpub.com/mapt/book/game_development/9781782162865 July 31, 2018.
+import pygame, sys
+from pygame.locals import *
+import numpy
