48f13990c8e9f1ddadb44a3bb3882ba9991d4a6d
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-03 14:20:34 -0700

    Import libraries.

--- accessing_sound_simple.py
+++ accessing_sound_simple.py
@@ -3,3 +3,5 @@
 # from the book "Instant Pygame for Python Game Development How-to" by Ivan Idris
 # accessed at https://www.packtpub.com/mapt/book/game_development/9781782162865 July 31, 2018.
 # critical modules for game funcition
+import pygame
+from pygame.locals import *
