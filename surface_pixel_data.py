af032c9155e54b32fd7f51af9810c59387e0ee6b
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-03 13:56:44 -0700

    ADD game loop. REVERT to smaller jpg file.

--- surface_pixel_data.py
+++ surface_pixel_data.py
@@ -8,17 +8,27 @@ from pygame.locals import *
 import numpy
 
 pygame.init()
-img = pygame.image.load('bax_head.jpg')
+# I tried using a different .jpg here but I think the file was too big.
+img = pygame.image.load('head.jpg')
 # surfarray handles the confersion between Surface and NumPy arrays
 # array2d copies pixels into a 2-d array
 pixels = pygame.surfarray.array2d(img)
-
 X = pixels.shape[0] * 7
 Y = pixels.shape[1] * 7
 screen = pygame.display.set_mode((X, Y))
 pygame.display.set_caption("Surfarray Demo")
-
 # tiles the image via tile function
 new_pixels = numpy.tile(pixels, (7, 7)).astype(int)
 # displays the array on the screen
 pygame.surfarray.blit_array(screen, new_pixels)
+
+while True:
+    screen.fill((255, 255, 255))
+    pygame.surfarray.blit_array(screen, new_pixels)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
