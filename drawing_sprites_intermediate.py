bf426edec6db28600b3c59fbe05c41369d5d1255
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-06 10:10:05 -0700

    ADD libraries.

--- drawing_sprites_intermediate.py
+++ drawing_sprites_intermediate.py
@@ -3,3 +3,6 @@
 # from the book "Instant Pygame for Python Game Development How-to" by Ivan Idris
 # accessed at https://www.packtpub.com/mapt/book/game_development/9781782162865 July 31, 2018.
 # critical modules for game funcition
+
+import os, pygame
+from pygame.locals import *
