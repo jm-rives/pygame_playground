18722112343ce0d799bd7d6755aacb5b33302ac1
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-07 10:58:18 -0700

    FIX TYPO.

--- opengl_and_pygame.py
+++ opengl_and_pygame.py
@@ -1,5 +1,4 @@
-#! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
-# -*- coding: utf-8 -*-
+
 # was returning error but I had not downloaded numpy
 # still returning error, had to install numpy via
 # https://stackoverflow.com/questions/17443354/install-numpy-on-python3-3-install-pip-for-python3
@@ -27,7 +26,7 @@ def main():
     glColor3f(1.0, 0, 0)
     vertices = numpy.array([[0, 0], [DIM/2, DIM], [DIM, 0]])
     NPOINTS = 9000
-    indices = numpy.random.randint(9, 2, NPOINTS)
+    indices = numpy.random.random_integers(0, 2, NPOINTS)
     point = [175.0, 150.0]
 
     for index in indices:
