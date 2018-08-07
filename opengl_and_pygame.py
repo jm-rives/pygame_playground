221cb653b9e9ac4d614c201e5398f3d2839942d3
Joanna M. Rives <joanna.rives@gmail.com>
2018-08-07 10:03:06 -0700

    ADD encoding.

---
+++ opengl_and_pygame.py
@@ -0,0 +1,5 @@
+#! /Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
+# -*- coding: utf-8 -*-
+# was returning error but I had not downloaded numpy
+# still returning error, had to install numpy via
+# https://stackoverflow.com/questions/17443354/install-numpy-on-python3-3-install-pip-for-python3
